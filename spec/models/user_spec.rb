93e782e478eaf2348eb18f7aedee51f123948255
Phan Quang Lam <lam.phan.qg@gmail.com>
2019-01-16 23:54:28 +0900

    Write some tests for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,36 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with an email, and password" do
+    user = User.new(email: "dummy@gmail.com", password: "abcde12345")
+    expect(user).to be_valid
+  end
+
+  it "is invalid if email is empty" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user).not_to be_valid
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid if email does not have email format" do
+    user = User.new(email: "abcde")
+    user.valid?
+    expect(user).not_to be_valid
+    expect(user.errors[:email]).to include("is invalid")
+  end
+
+  it "is invalid if password has less than 6 characters" do
+    user = User.new(email: "dummy@gmail.com", password: "12345")
+    user.valid?
+    expect(user).not_to be_valid
+    expect(user.errors[:password].join).to include("is too short")
+  end
+
+  it "is invalid if password has more than 128 characters" do
+    user = User.new(email: "dummy@gmail.com", password: "a" * 129)
+    user.valid?
+    expect(user).not_to be_valid
+    expect(user.errors[:password].join).to include("is too long")
+  end
 end
