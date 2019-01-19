1249a71ba27cd1223e39a58e8b62c08f05a80e76
Phan Quang Lam <lam.phan.qg@gmail.com>
2019-01-19 21:40:38 +0900

    Add some validations to Ridge model

--- app/models/ridge.rb
+++ app/models/ridge.rb
@@ -3,4 +3,23 @@
 class Ridge < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_many :species, through: :plants
+  validate :started_at_cannot_be_in_the_future,
+           :ended_at_cannot_be_in_the_future
+
+  def done?
+    !ended_at.nil? && Date.current > ended_at
+  end
+
+  private
+  def started_at_cannot_be_in_the_future
+    if !started_at.nil? && started_at > Date.current
+      errors.add(:started_at, "can't be in the future")
+    end
+  end
+
+  def ended_at_cannot_be_in_the_future
+    if !ended_at.nil? && ended_at > Date.current
+      errors.add(:ended_at, "can't be in the future")
+    end
+  end
 end
--- spec/models/ridge_spec.rb
+++ spec/models/ridge_spec.rb
@@ -3,5 +3,35 @@
 require "rails_helper"
 
 RSpec.describe Ridge, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(
+                email: "dummy@gmail.com",
+                password: "abcde12345"
+              ) }
+  let(:ridge) { Ridge.new(
+                user: user,
+                area: 5,
+                started_at: Date.current - 5.days,
+                ended_at: nil
+              ) }
+
+  it "is not done if ended_at is nil" do
+    expect(ridge.done?).to be false
+  end
+
+  it "is done if ended" do
+    ridge.ended_at = Date.yesterday
+    expect(ridge.done?).to be true
+  end
+
+  it "is invalid if started_at is in the future" do
+    ridge.started_at = Date.tomorrow
+    expect(ridge.valid?).to be false
+    expect(ridge.errors[:started_at]).to include("can't be in the future")
+  end
+
+  it "is invalid if ended_at is in the future" do
+    ridge.ended_at = Date.tomorrow
+    expect(ridge.valid?).to be false
+    expect(ridge.errors[:ended_at]).to include("can't be in the future")
+  end
 end
