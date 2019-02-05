c9d5164c7b110615aa3d044292a718cc052c59a5
Phan Quang Lam <lam.phan.qg@gmail.com>
2019-02-06 00:43:12 +0900

    Display flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,4 +28,15 @@ module ApplicationHelper
       link_to("ログイン", new_user_session_path)
     end
   end
+
+  def dimissible_flash(key, value)
+    key = "info" if key == "notice"
+    key = "danger" if key == "alert"
+    content_tag :div, class: "alert alert-#{key} alert-dismissible fade show", role: "alert" do
+      value.html_safe +
+      button_tag(type: "button", class: "close", "data-dismiss" => "alert", "aria-label" => "Close") do
+        content_tag(:span, "&times;".html_safe, "aria-hidden" => "true")
+      end
+    end
+  end
 end
