2066d69e3b4b2e9ac445da3c4bbd7f8174598ee5
Phan Quang Lam <lam.phan.qg@gmail.com>
2019-02-09 21:48:02 +0900

    Make admin_user? a common method for controllers and helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  helper_method :admin_user?
+
+  private
+    def admin_user?
+      user_signed_in? && current_user.is_admin?
+    end
 end
