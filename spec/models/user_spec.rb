bbbfad87d1fdb0d2ce9d4b8b1bbe197ca6684337
Phan Quang Lam <lam.phan.qg@gmail.com>
2019-03-12 05:55:30 +0900

    Change rspec expect messages due to introduction of ja.yml

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,27 +12,27 @@ RSpec.describe User, type: :model do
     user = User.new(email: nil)
     user.valid?
     expect(user).not_to be_valid
-    expect(user.errors[:email]).to include("can't be blank")
+    expect(user.errors[:email]).to include("を入力してください")
   end
 
   it "is invalid if email does not have email format" do
     user = User.new(email: "abcde")
     user.valid?
     expect(user).not_to be_valid
-    expect(user.errors[:email]).to include("is invalid")
+    expect(user.errors[:email]).to include("は不正な値です")
   end
 
   it "is invalid if password has less than 6 characters" do
     user = User.new(email: "dummy@gmail.com", password: "12345")
     user.valid?
     expect(user).not_to be_valid
-    expect(user.errors[:password].join).to include("is too short")
+    expect(user.errors[:password].join).to include("は6文字以上で入力してください")
   end
 
   it "is invalid if password has more than 128 characters" do
     user = User.new(email: "dummy@gmail.com", password: "a" * 129)
     user.valid?
     expect(user).not_to be_valid
-    expect(user.errors[:password].join).to include("is too long")
+    expect(user.errors[:password].join).to include("は128文字以内で入力してください")
   end
 end
