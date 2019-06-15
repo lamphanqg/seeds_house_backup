d0c78915b5040e9070967d6ed6ff3f4708773495
Phan Quang Lam <lam.phan.qg@gmail.com>
2019-06-15 19:38:51 +0900

    Remove PlantSeason

--- spec/features/species_spec.rb
+++ spec/features/species_spec.rb
@@ -3,34 +3,26 @@
 require "rails_helper"
 
 RSpec.feature "Species", type: :feature, js: true do
-  let(:cabbage_season) {
-    PlantSeason.create(
+  let(:cabbage) {
+    Species.create(name: "キャベツ",
+                   family: "アブラナ科",
       sow_months_start: 3,
       sow_months_end: 4,
       plant_months_start: 8,
       plant_months_end: 9,
       harvest_months_start: 16,
-      harvest_months_end: 19
-    )
+                   harvest_months_end: 19)
   }
 
-  let(:letuce_season) {
-    PlantSeason.create(
+  let(:letuce) {
+    Species.create(name: "レタス",
+                   family: "キク科",
       sow_months_start: 2,
       sow_months_end: 4,
       plant_months_start: 7,
       plant_months_end: 9,
       harvest_months_start: 14,
-      harvest_months_end: 16
-    )
-  }
-
-  let(:cabbage) {
-    Species.create(name: "キャベツ", family: "アブラナ科", plant_seasons: [cabbage_season])
-  }
-
-  let(:letuce) {
-    Species.create(name: "レタス", family: "キク科", plant_seasons: [letuce_season])
+                   harvest_months_end: 16)
   }
 
   scenario "display species list page" do
